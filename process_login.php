2af42c674d0232831e60a80652b0e21f1a11354e
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-26 14:58:06 +1000

    Added basic functionality to login

--- process_login.php
+++ process_login.php
@@ -1,17 +1,34 @@
 <?php
-session_start();
+    session_start();
+    // TODO: check to see if the input is not empty 
 	$username = $_POST['id'];
 	$password = $_POST['password'];
 
-	// TODO: connect to the db server
+    // Connect to the db server
+    $db = pg_connect("host=database-cc2020-a2.cjkzs400xcx4.us-east-1.rds.amazonaws.com dbname=artwork user=postgres password=FNH06upJc34i3hrm5h")
+    or die('Could not connect: ' . pg_last_error());
 
-	// TODO: define the query
+    // Define the query
+    $query = "select * from users where username = '$username' and password = '$password'";
 
-	// TODO: run an insert query
+    // Run an insert query
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    
 
-    // TODO: check to see if that user exist in database
-    if(true) {
+    // Check to see if that user exist in database
+    if(pg_num_rows($result) > 0) {
+        while ($row = pg_fetch_row($result)){
 
+            if($row[0] == $username && $row[4] == $password){
+                $_SESSION['username'] = $username;
+                header("Location:homepage.php");
+                exit(0);
+            }
+            // NOTE: This is not working yet, I'll have to look into it
+            else if ($row[0] != $username && $row[4] != $password){
+                print"<h3>The Username and Password is incorrect</h3>";
+            }
+        }
     }
     else {
         print"<h3>You are not registered</h3>";
