102731df06dc8cbf9528461936d5084234a2dd90
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-28 10:46:24 +1000

    Added artwork page
    This is for when you click on the art, it will load up the page to show the details, likes and comments

---
+++ artwork.php
@@ -0,0 +1,40 @@
+<?php
+// $db = pg_connect("host=database-cc2020-a2.cjkzs400xcx4.us-east-1.rds.amazonaws.com dbname=artwork user=postgres password=FNH06upJc34i3hrm5h")
+// or die('Could not connect: ' . pg_last_error());
+
+// $query = "select * from artwork where filename ='{$_GET['filename']}'";
+
+// $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// while ($row = pg_fetch_row($result)) {
+//     print "<p>ID: {$row['artwork_id']}</p>";
+//     print "<p>UserName: {$row['username']}</p>";
+//     print "<p>Work Title: {$row['title']}</p>";
+//     print "<p>Description: {$row['description']}</p>";
+//     echo "<div class='image'>";
+//     print "<img src='uploads/{$row['filename']}' />";
+//     echo "</div>";
+// }
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Artwork Page</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
