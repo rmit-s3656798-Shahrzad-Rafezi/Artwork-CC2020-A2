adb6cb860956fdb7fa1c8a33d6de2d40e376d4fb
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-30 09:35:22 +1000

    Added Design to add art and update file

--- add_art.php
+++ add_art.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['username'])) {
     header("Location:login.php");
     exit(0);
 } else {
-    print "<p id='f'>Welcome {$_SESSION['username']}</p>";
+    // print "<p id='f'>Welcome {$_SESSION['username']}</p>";
 }
 ?>
 
@@ -26,11 +26,41 @@ if (!isset($_SESSION['username'])) {
 </head>
 
 <body>
-    <div class="container d-flex align-items-center min-vh-100">
+    <div class="container add_art d-flex align-items-center min-vh-100">
 
-        <h1>Add your artwork</h1>
+        <div class="jumbotron">
+            <h1 class="text-center">Art Work</h1>
+        </div>
 
-        <form method="post" action="process_add_art.php" enctype=multipart/form-data>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href=homepage.php>Home</a></li>
+                    <li><a href=add_art.php>Add New Art</a></li>
+                    <li><a href="#">Page 2</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- <li><a href=register.php><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+                    <?php
+                    if (!isset($_SESSION['username'])) {
+                        print "<li><a href=register.php><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>";
+                        print "<li><a href=login.php><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
+                    } else {
+                        print "<li><a href=update.php><span class='glyphicon glyphicon-user'></span> Update Profile</a></li>";
+                        print "<li><a href=logout.php><span class='glyphicon glyphicon-log-in'></span> Logout</a></li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+        </nav>
+
+        <form method="post" action="process_add_art.php" enctype=multipart/form-data class="add_art_form">
+
+            <h1>Add your Artwork</h1>
+
             <div class="form-group">
                 <label>Title</label>
                 <input type="text" class="form-control" name="title" />
@@ -51,9 +81,9 @@ if (!isset($_SESSION['username'])) {
                 <input type="file" class="form-control" name="image" />
             </div>
 
-            <button type="submit" class="btn btn-default">Add Art</button>
+            <button type="submit" class="btn btn-default add_art">Add Art</button>
         </form>
-        
+
     </div>
 </body>
 
--- update.php
+++ update.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+// check if session variable exist
+if (!isset($_SESSION['username'])) {
+  header("Location:login.php");
+  exit(0);
+} else {
+  // print "<p id='f'>Welcome {$_SESSION['username']}</p>";
+}
+?>
+
 <!doctype html>
 <html>
 
@@ -16,22 +27,56 @@
 
 <body>
   <div class="container d-flex align-items-center min-vh-100">
-  <h1>Update</h1>
-    <form method="post" action="process_update.php">
+
+    <div class="jumbotron">
+      <h1 class="text-center">Art Work</h1>
+    </div>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href=homepage.php>Home</a></li>
+          <li><a href=add_art.php>Add New Art</a></li>
+          <li><a href="#">Page 2</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <?php
+          if (!isset($_SESSION['username'])) {
+            print "<li><a href=register.php><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>";
+            print "<li><a href=login.php><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
+          } else {
+            print "<li><a href=update.php><span class='glyphicon glyphicon-user'></span> Update Profile</a></li>";
+            print "<li><a href=logout.php><span class='glyphicon glyphicon-log-in'></span> Logout</a></li>";
+          }
+          ?>
+        </ul>
+      </div>
+    </nav>
+
+    <form method="post" action="process_update.php" class="update_form">
+
+      <h1>Update</h1>
 
       <div class="form-group">
         <label for="id">Username: </label>
         <input type="text" class="form-control" placeholder="Enter Username" name="id" required>
       </div>
 
-      <div class="form-group">
-        <label for="firstname">First Name: </label>
-        <input type="text" class="form-control" placeholder="Enter First Name" name="firstname">
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+          <label for="firstname">First Name: </label>
+          <input type="text" class="form-control" placeholder="Enter First Name" name="firstname">
+        </div>
       </div>
 
-      <div class="form-group">
-        <label for="lastname">Last Name: </label>
-        <input type="text" class="form-control" placeholder="Enter Last Name" name="lastname">
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+          <label for="lastname">Last Name: </label>
+          <input type="text" class="form-control" placeholder="Enter Last Name" name="lastname">
+        </div>
       </div>
 
       <div class="form-group">
@@ -65,16 +110,21 @@
         <input type="tel" class="form-control" placeholder="Enter phone number" name="phone" required>
       </div>
 
-      <div class="form-group">
-        <label for="password">Password: </label>
-        <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+          <label for="password">Password: </label>
+          <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
+        </div>
       </div>
 
-      <div class="form-group">
-        <label for="confirmPassword">Confirm Password: </label>
-        <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword" required>
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="form-group">
+          <label for="confirmPassword">Confirm Password: </label>
+          <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword" required>
+        </div>
       </div>
-      <button type="submit" class="btn btn-default">Update</button>
+
+      <button type="submit" class="btn btn-default update">Update</button>
     </form>
   </div>
 
