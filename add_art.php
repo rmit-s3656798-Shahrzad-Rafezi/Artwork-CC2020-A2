83b3ac47e170bfb27d01c9b61b62b52436eceb5c
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-05-04 14:00:29 +1000

    Created a nav.inc

--- add_art.php
+++ add_art.php
@@ -32,30 +32,9 @@ if (!isset($_SESSION['username'])) {
             <h1 class="text-center">Art Work</h1>
         </div>
 
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href=homepage.php>Home</a></li>
-                    <li><a href=add_art.php>Add New Art</a></li>
-                    <li><a href="#">Page 2</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- <li><a href=register.php><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
-                    <?php
-                    if (!isset($_SESSION['username'])) {
-                        print "<li><a href=register.php><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>";
-                        print "<li><a href=login.php><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
-                    } else {
-                        print "<li><a href=view_profile.php><span class='glyphicon glyphicon-user'></span> Profile</a></li>";
-                        print "<li><a href=logout.php><span class='glyphicon glyphicon-log-in'></span> Logout</a></li>";
-                    }
-                    ?>
-                </ul>
-            </div>
-        </nav>
+        <?php
+        include("nav.inc");
+        ?>
 
         <form method="post" action="process_add_art.php" enctype=multipart/form-data class="add_art_form">
 
--- view_profile.php
+++ view_profile.php
@@ -39,29 +39,10 @@ if (!isset($_SESSION['username'])) {
             <h1 class="text-center">Art Work</h1>
         </div>
 
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href=homepage.php>Home</a></li>
-                    <li><a href=add_art.php>Add New Art</a></li>
-                    <li><a href="#">Page 2</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <?php
-                    if (!isset($_SESSION['username'])) {
-                        print "<li><a href=register.php><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>";
-                        print "<li><a href=login.php><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
-                    } else {
-                        print "<li><a href=view_profile.php><span class='glyphicon glyphicon-user'></span> Profile</a></li>";
-                        print "<li><a href=logout.php><span class='glyphicon glyphicon-log-in'></span> Logout</a></li>";
-                    }
-                    ?>
-                </ul>
-            </div>
-        </nav>
+        <?php
+        include("nav.inc");
+        ?>
+
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6">
                 <?php
