b10abf4a17d6bcd19313d5befd0115b4b5982d64
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-05-12 18:55:35 +1000

    comment functionality half working
    NOTE: for some reason it prints one comment with just the userID, but when I reload the page, it creates another comment with userID and the text.

--- artwork.php
+++ artwork.php
@@ -1,12 +1,30 @@
 <?php
+session_start();
+
 $db = pg_connect("host=database-cc2020-a2.cjkzs400xcx4.us-east-1.rds.amazonaws.com dbname=artwork user=postgres password=FNH06upJc34i3hrm5h")
     or die('Could not connect: ' . pg_last_error());
 
-$query = "select * from artwork where imagename ='{$_GET['filename']}'";
-//$query = "select * from artwork where s3pathfile ='{$_GET['filename']}'";
+$get_artwork_query = "SELECT * FROM artwork WHERE imagename ='{$_GET['filename']}'";
+$get_artwork_result = pg_query($get_artwork_query) or die('Query failed: ' . pg_last_error());
 
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+$artwork_id_query = "SELECT id FROM artwork WHERE imagename ='{$_GET['filename']}'";
+$artwork_id_result = pg_query($artwork_id_query) or die('Query failed: ' . pg_last_error());
 
+while ($row = pg_fetch_row($artwork_id_result)) {
+    $comments_query = "SELECT * FROM comments WHERE artwork_id = {$row[0]}";
+    $comments = pg_query($comments_query) or die('Query failed: ' . pg_last_error());
+}
+
+$artwork_id_query1 = "SELECT id FROM artwork WHERE imagename ='{$_GET['filename']}'";
+$artwork_id_result1 = pg_query($artwork_id_query1) or die('Query failed: ' . pg_last_error());
+
+while ($row = pg_fetch_row($artwork_id_result1)) {
+    $artwork_id = (int) $row[0];
+    $comment_text = $_POST['comment_text'];
+    $username = $_SESSION['username'];
+    $query = "INSERT INTO comments (username, artwork_id, comment) VALUES ('$username', '$artwork_id', '$comment_text')";
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+}
 ?>
 
 <!doctype html>
@@ -35,10 +53,10 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
         <div class="col-xs-6 col-sm-6 col-md-6">
             <?php
-            while ($row = pg_fetch_row($result)) {
+            while ($artwork = pg_fetch_row($get_artwork_result)) {
                 //print "<img src='uploads/{$row[3]}' class='responsive' />";
-                print "<img src='{$row[4]}' class='responsive' />";
-                print "<p>{$row[2]}</p>";
+                print "<img src='{$artwork[4]}' class='responsive' />";
+                print "<p>{$artwork[2]}</p>";
             }
             ?>
             <a href="#" class="btn btn-info btn-lg">
@@ -48,24 +66,23 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
             <p class="number_of_likes">5</p>
         </div>
 
-        <!-- <div class="col-xs-6 col-sm-6 col-md-6">
-            <img src="module-6.jpg" alt="" class="responsive">
-
-            <a href="#" class="btn btn-info btn-lg">
-                <span class="glyphicon glyphicon-thumbs-up"></span> Like
-            </a>
-
-            <p class="number_of_likes">5</p>
-        </div> -->
-
         <div class="col-xs-6 col-sm-6 col-md-6">
 
-            <form action="post_details.php" method="post" id="comment_form">
+            <form method="post" id="comment_form">
                 <textarea name="comment_text" id="comment_text" class="form-control" cols="3" rows="3" style="margin-bottom: 5px"></textarea>
                 <button class="btn btn-primary btn-sm pull-right" id="submit_comment">Submit comment</button>
             </form>
 
-            <div class="comment" style="margin-top: 40px">
+            <?php
+            while ($comment = pg_fetch_row($comments)) {
+                echo "<div class='comment'>";
+                print "<h5>{$comment[1]}</h5>";
+                print "<p>{$comment[3]}</p>";
+                echo "</div>";
+            }
+            ?>
+
+            <!-- <div class="comment" style="margin-top: 40px">
                 <h5>Mark Dang</h5>
                 <p>Oh wow I love your art work</p>
                 <span class="time-right">11:00</span>
@@ -74,13 +91,11 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
                 <h5>Gemma Ruse</h5>
                 <p>This is truly amazing</p>
                 <span class="time-right">12:00</span>
-            </div>
+            </div> -->
         </div>
 
-
-
     </div>
-
+    <!-- <script src="scripts.js"></script> -->
 </body>
 
 </html>
